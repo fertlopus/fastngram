10497c2ab50aaaec221cd5b5cef081352c6cc772
fertlopus <marat.movlamov@icloud.com>
2023-07-09 01:56:47 +0300

    Preprocessing final variant

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -16,12 +16,6 @@ REPLACE_DICTIONARY = {
 
 
 def process_line(line: typing.AnyStr):
-    """
-    Function to preprocess the text file. Replace the character
-    in read line and joining them into one array.
-    :param line: str
-    :return: an array of preprocessed strings
-    """
     line = line.strip()
     for old_character, new_character in REPLACE_DICTIONARY.items():
         line = line.replace(old_character, new_character)
@@ -30,3 +24,41 @@ def process_line(line: typing.AnyStr):
     lines = [l.strip() for l in line.split('\n') if l.strip() != '']
     return lines
 
+
+def process_corpora(configuration):
+    processed_corpus = []
+    with open(configuration.corpora_path, mode='r', encoding=configuration.encoding, errors='ignore') as file:
+        lines = file.readlines()
+    if configuration.verbose:
+        bar = tqdm(desc="Replacing spaces into another character.",  total=len(lines), mininterval=1,
+                   bar_format="{desc}: {percentage:3.0f}% ({remaining} left")
+    for line in lines:
+        if configuration.verbose:
+            bar.update()
+        processed_lines = process_line(line)
+        processed_corpus.extend(processed_lines)
+
+    processed_corpus = '_'.join(processed_corpus)
+    processed_corpus = '_'.join(processed_corpus.split())
+    processed_corpus = processed_corpus.strip()
+
+    with open(configuration.processed_path, mode='w', encoding=configuration.encoding, errors='ignore') as final_file:
+        final_file.write(processed_corpus)
+
+    if configuration.verbose:
+        bar.close()
+
+    file.close()
+    final_file.close()
+    print("Finished.\n")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='WNE_embeddings/preprocessing/main.py')
+    parser.add_argument('--corpora_path', type=str, default='../data/raw/sample.txt', help='Corpora Path in str format')
+    parser.add_argument('--processed_path', type=str, default='../data/processed/processed.txt', help='Output in str')
+    parser.add_argument('--encoding', type=str, default='utf-8', help='Encoding for the text file. Default is utf-8')
+    parser.add_argument('--verbose', type=bool, default=True, help='Verbose option')
+    configuration = parser.parse_args()
+    print(configuration)
+    process_corpora(configuration=configuration)
