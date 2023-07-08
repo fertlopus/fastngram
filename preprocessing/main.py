d8417cd08f5a1e68d4b2ba033e2e915fca8cec9b
fertlopus <marat.movlamov@icloud.com>
2023-07-09 01:37:56 +0300

    Process line function for the text.

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -2,6 +2,7 @@
 import os
 import argparse
 from tqdm import tqdm
+import typing
 
 
 REPLACE_DICTIONARY = {
@@ -12,4 +13,20 @@ REPLACE_DICTIONARY = {
     '\u200a': ' ', '\u2028': ' ', '\u2029': ' ', '\u202f': ' ', '\u205f': ' ',
     '\u3000': ' ',
 }
+
+
+def process_line(line: typing.AnyStr):
+    """
+    Function to preprocess the text file. Replace the character
+    in read line and joining them into one array.
+    :param line: str
+    :return: an array of preprocessed strings
+    """
+    line = line.strip()
+    for old_character, new_character in REPLACE_DICTIONARY.items():
+        line = line.replace(old_character, new_character)
+    line = ' '.join(line.split())
+    line = '\n'.join(line.split('\n'))
+    lines = [l.strip() for l in line.split('\n') if l.strip() != '']
+    return lines
 
